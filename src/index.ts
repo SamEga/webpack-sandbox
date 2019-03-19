4a6cf97615c4d3b0e2dc6bee3f631ae3690fbb54
SamEga <egorstepanow@mail.ru>
2019-03-19 18:18:13 +0700

    first version

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// import './style.css';
+import printMe from './print.js';
+
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
